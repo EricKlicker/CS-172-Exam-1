f4a479215c3eeaa1fd58ff0f2e2d6201596b0f74
Eric Klicker <ericklicker@yahoo.com>
2016-10-05 21:56:36 -0700

    Initial Start

---
+++ CS 172 Exam 1/CS 172 Exam 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS 172 Exam 1
+//
+//  Created by Eric Klicker on 10/5/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
